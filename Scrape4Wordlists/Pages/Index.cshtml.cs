83d93789021547f1b76ef8b8d8a6ff27f28148aa
iambase <30421546+iambase@users.noreply.github.com>
2020-06-21 15:21:59 +0100

    Initial Commit

---
+++ Scrape4Wordlists/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Scrape4Wordlists.Pages
+{
+    public class IndexModel : PageModel
+    {
+        //private ScraperContext _context;
+
+        public int ScrapedUriCount { get; private set; } = 0;
+
+        //public IndexModel(ScraperContext context)
+        //{
+        //    _context = context;
+        //}
+
+        public void OnGet()
+        {
+            //try
+            //{
+            //    ScrapedUriCount = _context.ScrapeUri.Count();
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e);
+            //}
+
+        }
+    }
+}
